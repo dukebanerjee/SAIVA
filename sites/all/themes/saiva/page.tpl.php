a323f3c068ab796ec9aac44610b90db5170fadec
Duke Banerjee <dbanerje@sbcglobal.net>
2012-02-07 20:36:24 -0600

    Add page tabs

--- sites/all/themes/saiva/page.tpl.php
+++ sites/all/themes/saiva/page.tpl.php
@@ -37,7 +37,13 @@
   <div id="columns-top"></div>
   <div id="columns">
     <div id="content">
+      <?php if ($page['highlighted']): ?><div id="highlighted"><?php print render($page['highlighted']); ?></div><?php endif; ?>
+      <a id="main-content"></a>
+      <?php if ($tabs): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
+      <?php print render($page['help']); ?>
+      <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
       <?php print render($page['content']); ?>
+      <?php print $feed_icons; ?>
     </div>
     <div id="footer1">
       <div id="footer1-gutter-left"></div>
