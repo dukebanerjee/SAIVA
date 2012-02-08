1e104cfac9896fbae31a934a77263f4958e00eff
Duke Banerjee <dbanerje@sbcglobal.net>
2012-02-07 20:53:05 -0600

    Hide title on front page

--- sites/all/themes/saiva/page.tpl.php
+++ sites/all/themes/saiva/page.tpl.php
@@ -39,6 +39,11 @@
     <div id="content">
       <?php if ($page['highlighted']): ?><div id="highlighted"><?php print render($page['highlighted']); ?></div><?php endif; ?>
       <a id="main-content"></a>
+      <?php if(!$is_front): ?>
+      <?php print render($title_prefix); ?>
+      <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
+      <?php print render($title_suffix); ?>
+      <?php endif; ?>
       <?php if ($tabs): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
       <?php print render($page['help']); ?>
       <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
