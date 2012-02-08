165b071ae6d1a2b7eea188c8d9ee3d2e3aac6c2c
Duke Banerjee <dbanerje@sbcglobal.net>
2012-02-07 21:14:19 -0600

    Header style

--- sites/all/themes/saiva/page.tpl.php
+++ sites/all/themes/saiva/page.tpl.php
@@ -41,7 +41,7 @@
       <a id="main-content"></a>
       <?php if(!$is_front): ?>
       <?php print render($title_prefix); ?>
-      <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
+      <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?>:</h1><?php endif; ?>
       <?php print render($title_suffix); ?>
       <?php endif; ?>
       <?php if ($tabs): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
